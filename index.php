a375a311454386632933d70117b48322ccada071
Veciti42 <katici95@gmail.com>
2020-12-21 21:57:50 +0100

    4i u toku

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+   $servername = "127.0.0.1";
+    $username = "root";
+    $password = "vivify";
+    $dbname = "blog";
+    try {
+        $connection = new PDO("mysql:host = $servername; dbname = $dbname", $username, $password);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+?>
 
 <!doctype html>
 <html lang="en">
